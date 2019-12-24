7c0b59842eeb0f06ec9594d3fc23509ae853cce9
dongjuppp <dongjuppp@gmail.com>
2019-12-25 00:30:05 +0900

    pix

--- s001/perceptron.py
+++ s001/perceptron.py
@@ -21,7 +21,7 @@ class Perceptron(object):
         에포크마다 누적된 분류 오류
 
     """
-    def __init__(self,eta = 0.01,n_iter=50, random_state = 1):
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
